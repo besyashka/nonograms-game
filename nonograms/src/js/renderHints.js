dea087b4281e0ecc0c715e6c54765a0296c77fb0
Ekaterina Yurina <komsa_9191@mail.ru>
2025-02-07 15:22:13 +1000

    feat: add file js with hint rendering functions

---
+++ nonograms/src/js/renderHints.js
@@ -0,0 +1,60 @@
+import { template } from './template.js';
+import { createElement } from './renderPage.js';
+
+// выбрать случайный шаблон для игры
+export const getRandomTemplate = () => {
+  let index = Math.floor(Math.random() * template.length);
+  const randomTemplate = template[index];
+  console.log(randomTemplate.name);
+
+  renderHintsLeft(randomTemplate);
+  renderHintsTop(randomTemplate);
+};
+
+// добавить исловые подсказки слева
+const renderHintsLeft = (template) => {
+  const hintsLeft = document.querySelectorAll('.hint-left');
+  const templateField = template.field;
+
+  for (let row = 0; row < templateField.length; row++) {
+    let counter = 0;
+
+    for (let col = 0; col < templateField[row].length; col++) {
+      if (templateField[row][col]) {
+        counter++;
+      } else if (counter > 0) {
+        createElement('div', 'number-hint-left', hintsLeft[row], counter);
+        counter = 0;
+      }
+    }
+
+    if (counter > 0) {
+      createElement('div', 'number-hint-left', hintsLeft[row], counter);
+      counter = 0;
+    }
+  }
+};
+
+// добавить числовые подсказки вверху
+const renderHintsTop = (template) => {
+  const hintsTop = document.querySelectorAll('.hint-top');
+  const templateField = template.field;
+
+  for (let row = 0; row < templateField.length; row++) {
+    let counter = 0;
+
+    for (let col = 0; col < templateField[row].length; col++) {
+      if (templateField[col][row]) {
+        counter++;
+      } else if (counter > 0) {
+        createElement('div', 'number-hint-top', hintsTop[row], counter);
+        counter = 0;
+      }
+    }
+
+    if (counter > 0) {
+      createElement('div', 'number-hint-top', hintsTop[row], counter);
+      counter = 0;
+    }
+  }
+};
